70d3bf4170bd749eb5e526320cd301809081d8a9
Magomed <qwertyhenrytino@gmail.com>
2023-09-01 15:20:41 +0300

    first

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+import sys
+
+host = input("Host: ")
+port = int(input("Port: "))
+
+try:
+    # Подключаемся к серверу
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+except:
+    print("Невозможно подключиться к серверу. Проверьте правильность указания хоста и порта")
+    input("Press enter to quit")
+    sys.exit(0)
+
+while True:
+    # Клиент отправляет сообщение
+    message = input()
+    sock.sendall(str.encode(message))
