cf249485b835466d2f8e6f91fefce2f60f3e4a50
warnaz <124733559+warnaz@users.noreply.github.com>
2023-09-11 14:07:41 +0300

    Update client.py

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ try:
     # Подключаемся к серверу
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
+    print(f"[CLIENT] Listening on {host}: {port}")
 except:
     print("Невозможно подключиться к серверу. Проверьте правильность указания хоста и порта")
     input("Press enter to quit")
@@ -16,5 +17,5 @@ except:
 
 while True:
     # Клиент отправляет сообщение
-    message = input()
+    message = input("Введите сообщение: ")
     sock.sendall(str.encode(message))
