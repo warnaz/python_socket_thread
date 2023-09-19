50538ae4f9cf7db615768e9f1fd7021e7460988e
Magomed <negotiumnomen@gmail.com>
2023-09-19 18:50:00 +0300

    fix

--- binance_server/server.py
+++ binance_server/server.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import asyncio
 
 app = FastAPI()
 
+
 class RequestData(BaseModel):
     data: dict
+
+
+def process_data_sync(data):
+    # Обрабатываем данные
+    from stream import main
+    main(data)
+
+
+async def process_data_async(data):
+    await asyncio.to_thread(process_data_sync, data)
 
 
 @app.post("/process_data")
 async def process_data(request_data: RequestData):
     # Получаем данные из запроса
     data = request_data.data
-    print(data)
 
-    # Обрабатываем данные
-    from stream import main
-    main(data)
+    print('------- Подключение клиента -------', end='\n')
+
+    # Запускаем обработку данных асинхронно
+    await process_data_async(data)
 
     return {"message": "Данные обработаны и успешно отправлены"}
 
